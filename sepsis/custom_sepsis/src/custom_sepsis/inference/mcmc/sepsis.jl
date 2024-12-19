c98d5caa32ddc9a292bd0d092a57141c99da1e94
Luisa <luisa.stueckelberger@uzh.ch>
2024-12-19 12:59:36 +0100

    wip

--- sepsis/custom_sepsis/src/custom_sepsis/inference/mcmc/sepsis.jl
+++ sepsis/custom_sepsis/src/custom_sepsis/inference/mcmc/sepsis.jl
@@ -1,10 +1,11 @@
 
 
 module Sepsis
-export labeled_categorical, get_reward, random_initial_state, get_next_state, simulate_episode, get_parameters, sepsis_model
+export labeled_categorical, get_reward, random_initial_state, get_next_state, simulate_episode, get_parameters, sepsis_model, hr_probs, bp_probs, o2_probs, glu_probs
 
 using Gen
 using ..SepsisTypes
+using LinearAlgebra
 
 
 @dist function labeled_categorical(labels, probs)
@@ -39,47 +40,47 @@ end
 softmax(vector) = exp.(vector) / sum(exp.(vector))
 add_0(vector) = [vector; 0]
 
-function hr_probs(parameters, state, action)
+function hr_probs(parameters::Parameters, state::State, action::Action)
     hr_probs = softmax(add_0(
-        parameters[:hr=>state.hr=>:intercept]
+        parameters[(:hr, state.hr, :intercept)]
         .+
-        parameters[:hr=>state.hr=>:abx] .* action[:abx]
+        parameters[(:hr, state.hr, :abx)] .* Int(action.abx)
         .+
-        parameters[:hr=>state.hr=>:vaso] .* action[:vaso]
+        parameters[(:hr, state.hr, :vaso)] .* Int(action.vaso)
     ))
     return hr_probs
 end
 
-function bp_probs(parameters, state, action)
+function bp_probs(parameters::Parameters, state::State, action::Action)
     bp_probs = softmax(add_0(
-        parameters[:bp=>state.bp=>:intercept]
+        parameters[(:bp, state.bp, :intercept)]
         .+
-        parameters[:bp=>state.bp=>:abx] .* action[:abx]
+        parameters[(:bp, state.bp, :abx)] .* Int(action.abx)
         .+
-        parameters[:bp=>state.bp=>:vaso] .* action[:vaso]
+        parameters[(:bp, state.bp, :vaso)] .* Int(action.vaso)
     ))
     return bp_probs
 end
 
-function o2_probs(parameters, state, action)
+function o2_probs(parameters::Parameters, state::State, action::Action)
     o2_probs = softmax(add_0(
-        parameters[:o2=>state.o2=>:intercept]
+        parameters[(:o2, state.o2, :intercept)]
         .+
-        parameters[:o2=>state.o2=>:vent] .* action[:vent]
+        parameters[(:o2, state.o2, :vent)] .* Int(action.vent)
     ))
     return o2_probs
 end
 
-function glu_probs(parameters, state, action)
+function glu_probs(parameters::Parameters, state::State, action::Action)
     glu_probs = softmax(add_0(
-        parameters[:glu=>state.glu=>:intercept]
+        parameters[(:glu, state.glu, :intercept)]
         .+
-        parameters[:glu=>state.glu=>:vaso] .* action[:vaso]
+        parameters[(:glu, state.glu, :vaso)] .* Int(action.vaso)
     ))
     return glu_probs
 end
 
-@gen function get_next_state(params::Dict, state::State, action::Action)::State
+@gen function get_next_state(params::Parameters, state::State, action::Action)::State
     hr ~ labeled_categorical(HR_LEVELS, hr_probs(params, state, action))
     bp ~ labeled_categorical(BP_LEVELS, bp_probs(params, state, action))
     o2 ~ labeled_categorical(O2_LEVELS, o2_probs(params, state, action))
@@ -88,7 +89,7 @@ end
     return next_state
 end
 
-@gen function simulate_episode(beliefs::Dict, policy::Policy, start_state::State)
+@gen function simulate_episode(beliefs::Parameters, policy::Policy, start_state::State)
     states = [start_state]
     state = start_state
     rewards = []
@@ -105,43 +106,43 @@ end
     return states, rewards
 end
 
-@gen function get_parameters()::Dict
+@gen function get_parameters()::Parameters
 
-    parameters = Dict()
+    parameters = Parameters()
 
     for level in HR_LEVELS
-        parameters[:hr=>level=>:intercept] =
+        parameters[(:hr, level, :intercept)] =
             {:hr => level => :intercept} ~ mvnormal([0, 0], Diagonal([1.0, 1.0]))
-        parameters[:hr=>level=>:abx] =
+        parameters[(:hr, level, :abx)] =
             {:hr => level => :abx} ~ mvnormal([0, 0], Diagonal([1.0, 1.0]))
-        parameters[:hr=>level=>:vaso] =
+        parameters[(:hr, level, :vaso)] =
             {:hr => level => :vaso} ~ mvnormal([0, 0], Diagonal([1.0, 1.0]))
     end
 
     for level in BP_LEVELS
-        parameters[:bp=>level=>:intercept] =
+        parameters[(:bp, level, :intercept)] =
             {:bp => level => :intercept} ~ mvnormal([0, 0], Diagonal([1.0, 1.0]))
-        parameters[:bp=>level=>:abx] =
+        parameters[(:bp, level, :abx)] =
             {:bp => level => :abx} ~ mvnormal([0, 0], Diagonal([1.0, 1.0]))
-        parameters[:bp=>level=>:vaso] =
+        parameters[(:bp, level, :vaso)] =
             {:bp => level => :vaso} ~ mvnormal([0, 0], Diagonal([1.0, 1.0]))
     end
 
     for level in O2_LEVELS
-        parameters[:o2=>level=>:intercept] =
+        parameters[(:o2, level, :intercept)] =
             {:o2 => level => :intercept} ~ mvnormal([0], Diagonal([1.0]))
-        parameters[:o2=>level=>:vent] =
+        parameters[(:o2, level, :vent)] =
             {:o2 => level => :vent} ~ mvnormal([0], Diagonal([1.0]))
     end
 
     for level in GLU_LEVELS
-        parameters[:glu=>level=>:intercept] =
-            {:glu => level => :intercept} ~ mvnormal([0, 0], Diagonal([1.0, 1.0]))
-        parameters[:glu=>level=>:vaso] =
-            {:glu => level => :vaso} ~ mvnormal([0, 0], Diagonal([1.0, 1.0]))
+        parameters[(:glu, level, :intercept)] =
+            {:glu => level => :intercept} ~ mvnormal([0, 0, 0, 0], Diagonal([1.0, 1.0, 1.0, 1.0]))
+        parameters[(:glu, level, :vaso)] =
+            {:glu => level => :vaso} ~ mvnormal([0, 0, 0, 0], Diagonal([1.0, 1.0, 1.0, 1.0]))
     end
 
-
+    return parameters
 end
 
 @gen function sepsis_model(policies::Vector{Policy}, start_states::Vector{State})
